64fb4f99f16e75bfd38d4e3c630dd63b0457e4e7
Christian Vogel <vogelchr@vogel.cx>
2022-01-07 13:57:49 +0100

    No longer create extra thread for pygame, just poll for events.
    This makes it no loger segfault.

--- hw_pygame.py
+++ hw_pygame.py
@@ -5,9 +5,6 @@ from logging import critical, debug, error, info, warning
 import PIL.Image
 import pygame
 
-# inspired by
-# https://github.com/AlexElvers/pygame-with-asyncio/blob/master/main.py
-
 
 class HW_PyGame:
     def __init__(self, loop, width, height):
@@ -15,41 +12,28 @@ class HW_PyGame:
         self.width, self.height = width, height
         self.running = True
 
-        # we'll get pygame events out of this queue
-        self.event_queue = asyncio.Queue()
-
         pygame.init()
         self.window = pygame.display.set_mode((3*self.width, 3*self.height))
-        self.pygame_thread = loop.run_in_executor(
-            None, self._pygame_thread_fct)
-
-        # events will be consumed by this task
         self.evt_consumer = loop.create_task(self._evt_consumer_coro())
 
     # stop the 'HW'
     def stop(self):
-        self.pygame_thread.cancel()
-        pygame.quit()  # this segfaults ;-)
+        self.running = False
+        pygame.quit()
 
-    # this runs in a seperate thread
-    def _pygame_thread_fct(self):
-        while True:
-            event = pygame.event.wait()
-            asyncio.run_coroutine_threadsafe(
-                self.event_queue.put(event), loop=self.loop)
-
-    # this runs within the asyncio framework
     async def _evt_consumer_coro(self):
-        while True:
-            event = await self.event_queue.get()
+        while self.running:
+            event = pygame.event.poll()
+            if event.type == pygame.locals.NOEVENT:
+                await asyncio.sleep(0.01)
+                continue
+            debug(f'pygame Event {repr(event)} received.')
             if event.type == pygame.locals.QUIT:
                 info('Window has been closed, exiting.')
                 self.running = False
-                break
             if event.type == pygame.locals.KEYUP and event.key == pygame.locals.K_ESCAPE:
                 info('ESC has been presed, exiting.')
                 self.running = False
-                break
 
     # update pixel matrix from PIL Image
     def update(self, img):
@@ -59,6 +43,7 @@ class HW_PyGame:
         rect = pygame.Rect(0, 0, 3*self.width, 3*self.height)
         pygame.draw.rect(self.window, (0x55, 0x55, 0x55), rect)
 
+        # this is very, very inefficient
         for x in range(min(self.width, img.size[0])):
             for y in range(min(self.height, img.size[1])):
                 if img.getpixel((x, y)):
