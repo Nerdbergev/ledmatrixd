a21fa49a9ce3b4e8afb0f8431559b97310c8ae8e
Christian Vogel <vogelchr@vogel.cx>
2022-01-22 11:21:13 +0100

    move venv generation in the run... scripts.

--- gen_venv.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-virtualenv ./venv
-./venv/bin/pip install pillow pygame asyncio-mqtt pyusb
--- run_ledmatrixd_sim.sh
+++ run_ledmatrixd_sim.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 [ -d venv ] || ./gen_venv.sh
-exec ./ledmatrixd.py \
+
+virtualenv ./venv
+./venv/bin/pip install pillow pygame asyncio-mqtt
+
+exec ./venv/bin/python ledmatrixd.py \
 	-S \
 	-f /usr/share/fonts/misc/ter-x20b.pcf.gz \
 		/usr/share/fonts/misc/5x7.pcf.gz \
